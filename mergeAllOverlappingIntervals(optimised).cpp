93c10c5fc24933aa69025bee3327aec088512c02
anushi12 <113124216+anushi12@users.noreply.github.com>
2023-06-11 20:53:14 +0530

    Create mergeAllOverlappingIntervals(optimised).cpp

---
+++ mergeAllOverlappingIntervals(optimised).cpp
@@ -0,0 +1,19 @@
+//optimised
+
+#include<vector>
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
+	// Write your code here.
+	int n = arr.size();
+	sort(arr.begin(),arr.end());
+	vector<vector<int>>ans;
+	for(int i=0;i<n;i++){
+		if(ans.empty() || arr[i][0]>ans.back()[1]){
+			ans.push_back(arr[i]);
+		}
+		else{
+			ans.back()[1] = max(ans.back()[1],arr[i][1]);
+		}
+	}
+	return ans;
+}
