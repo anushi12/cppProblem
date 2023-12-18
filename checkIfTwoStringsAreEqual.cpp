7a0ea1a9f07ebdcba59b1fdf602fc6e5c4d5f73f
anushi12 <113124216+anushi12@users.noreply.github.com>
2023-12-18 21:53:57 +0530

    Create checkIfTwoStringsAreEqual.cpp

---
+++ checkIfTwoStringsAreEqual.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string ans1, ans2;
+        for(int i = 0; i<word1.size();i++){
+             ans1 += word1[i];
+        }
+        for(int i = 0; i<word2.size();i++){
+             ans2 += word2[i];
+        }
+        return ans1 == ans2;
+    }
+};
