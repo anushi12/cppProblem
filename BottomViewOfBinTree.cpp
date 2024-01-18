fba4bd4e9ac5f0bf6c2ae6eeb6504234e6272b0d
anushi12 <113124216+anushi12@users.noreply.github.com>
2024-01-18 18:36:23 +0530

    Create BottomViewOfBinTree.cpp

---
+++ BottomViewOfBinTree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        // Your Code Here
+        map<int,int>mp;
+        queue<pair<Node*, int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            Node* node = it.first;
+            int line = it.second;
+            mp[line] = node->data;
+            if(node->left){
+                q.push({node->left, line-1});
+            }
+            if(node->right){
+                q.push({node->right, line +1});
+            }
+        }
+        vector<int>ans;
+        for(auto it: mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
